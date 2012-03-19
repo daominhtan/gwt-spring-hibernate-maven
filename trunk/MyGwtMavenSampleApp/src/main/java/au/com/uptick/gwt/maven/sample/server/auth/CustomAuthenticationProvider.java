ad51d48824181e8a5b3cd3880f695d64fd272211
damianciocca@gmail.com <damianciocca@gmail.com@ceb15c4f-bd34-38b4-0f09-19fcd725dad2>
2012-03-19 16:09:14 +0000

    se agrega el form de login. se quita una clase q estaba al pedo. se agrega nuevos interceptores de securizacion en el applicationContext-security.xml

--- trunk/MyGwtMavenSampleApp/src/main/java/au/com/uptick/gwt/maven/sample/server/auth/CustomAuthenticationProvider.java
+++ trunk/MyGwtMavenSampleApp/src/main/java/au/com/uptick/gwt/maven/sample/server/auth/CustomAuthenticationProvider.java
@@ -19,6 +19,15 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		users.put("justin", "javacodegeeks");
 	}
 
+	/**
+	 * The authenticate method is implemented and inside that we retrieve the username provided in the login form 
+	 * (via the getPrincipal method) as well as the accompanying password (via the getCredentials method). 
+	 * First, we check if the specific username exists and if not, a UsernameNotFoundException is thrown. 
+	 * Similarly, if the username exists but the password is incorrect, a BadCredentialsException is thrown. 
+	 * Note that both theses exceptions extend the parent AuthenticationException class.
+	 * 
+	 * Through form login.html, springsecurity redirect to this method (authenticate) using "j_spring_security_check" servlet (action="<c:url value='j_spring_security_check' />") 
+	 */
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		
 		System.out.println("CustomAuthenticationProvider => authenticate() ");
@@ -26,6 +35,9 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		String username = (String) authentication.getPrincipal();
 		String password = (String)authentication.getCredentials();
 		
+		/*
+		 * Go to BD to check if this user and pass exists.
+		 */
 		if (users.get(username)==null)
 			throw new UsernameNotFoundException("User not found");
 		
@@ -34,13 +46,32 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 		if (!storedPass.equals(password))
 			throw new BadCredentialsException("Invalid password");
 		
+		/* 
+		 * If both the username and the password are correct, we are in place to authenticate the user. 
+		 * In order to do so, we have to return a concrete instance of the Authentication interface. 
+		 * In that, we have to encapsulate the already known user information (credentials etc.) as well as the roles (authorities) 
+		 * that the user has. 
+		 * Note that the assigned role (ROLE_USER) matches the one declared in the “applicationContext-security.xml” file.		
+		 * 
+		 * Read more: http://www.javacodegeeks.com/2010/12/securing-gwt-apps-with-spring-security.html#ixzz1pZXiobcO
+		 */
 		Authentication customAuthentication = new CustomUserAuthentication("ROLE_USER", authentication);
+		
+		/*
+		 * In addition, the setAuthenticated method has to be invoked (with true as argument) in order to indicate to the rest 
+		 * of the authentication chain that the specific user was successfully authenticated by our module
+		 */
 		customAuthentication.setAuthenticated(true);
 		
 		return customAuthentication;
 		
 	}
 
+	/**
+	 * The supports method defines the kind of authentication that this provider provides. 
+	 * In our case, the UsernamePasswordAuthenticationToken is the one we wish to handle. That implementation is designed for 
+	 * simple presentation of a username and password.
+	 */
 	public boolean supports(Class<? extends Object> authentication) {
 		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
 	}
