df3469a18a10382d6d9017c7017bcf036cbd3f0a
damianciocca@gmail.com <damianciocca@gmail.com@ceb15c4f-bd34-38b4-0f09-19fcd725dad2>
2012-05-17 19:47:49 +0000

    Se implementa el nuevo framework de GWT 2.1 de Activities & Places.

--- trunk/MyGwtMavenSampleApp/src/main/java/au/com/uptick/gwt/maven/sample/client/auth/event/UpdatedRoleEventHandler.java
+++
@@ -1,9 +0,0 @@
-package au.com.uptick.gwt.maven.sample.client.auth.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface UpdatedRoleEventHandler extends EventHandler{
-
-	void onUpdatedRole(UpdatedRoleEvent event);
-
-}
