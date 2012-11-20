87d878f65b2aee31f09160cc707c3ef68fafa72f
damianciocca <damianciocca@gmail.com>
2012-11-20 20:29:46 +0000

    GEMALTO VTR CHILE CUSTOM => Cliente Ejemplo con WSIMPORT

---
+++ WSProject/src/main/java/com/gemalto/latamsoft/vtr/chile/ws/client/VTRApiSOAPException.java
@@ -0,0 +1,62 @@
+
+package com.gemalto.latamsoft.vtr.chile.ws.client;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for VTRApiSOAPException complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="VTRApiSOAPException">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="faultCode" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "VTRApiSOAPException", propOrder = {
+    "faultCode"
+})
+public class VTRApiSOAPException {
+
+    @XmlElement(required = true, nillable = true)
+    protected String faultCode;
+
+    /**
+     * Gets the value of the faultCode property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFaultCode() {
+        return faultCode;
+    }
+
+    /**
+     * Sets the value of the faultCode property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFaultCode(String value) {
+        this.faultCode = value;
+    }
+
+}
